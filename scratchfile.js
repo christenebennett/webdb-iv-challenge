83580ce54a8a2e2018436d75815af9d45676b87c
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-20 22:53:28 -0700

    Initial commit

---
+++ scratchfile.js
@@ -0,0 +1,28 @@
+dish 
+
+{id: 1, name: tacos}
+{id: 2, name: lasagna}
+  
+
+recipes
+{
+  "id": 1, 
+  "name": "shredded beef tacos", 
+  "dish_id": 1
+  "ingredients": [
+    {
+      ingredient_id: 1
+      ingredient: taco shells
+      qty: 8
+    },
+    {
+      ingredient_id: 2,
+      ingredient: pound of shredded beef,
+      qty: 1
+    }
+    {
+      ingredient_id: 3
+      ingredient: cup of shredded lettuce
+      qty: 1
+    }
+  ] }
